f336e858ea47ef0d00189956552d75879aa5bd40
Mohamed Fathy <codetwostart@gmail.com>
2019-11-16 12:50:40 +0200

    initial commit

---
+++ app.dart
@@ -0,0 +1,13 @@
+sayHello() {
+  print("Hello from Say");
+}
+
+main() {
+  //print("Hello developers from main method");
+  /*I will print 
+  hello 
+  developer
+  */
+  ///single line
+  /** multi-line documentation comment */
+}
